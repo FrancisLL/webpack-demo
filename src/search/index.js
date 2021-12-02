904dcd982b73279c4f73923f1928bd1a65e1eeb1
heavyrain <lizhipeng@rongcloud.cn>
2021-12-02 08:55:12 +0800

    feat: dynamic import

--- src/search/index.js
+++ src/search/index.js
@@ -4,14 +4,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './search.less'
 import logo from './images/logo.jpg'
-import '../../common'
+import { common } from '../../common'
 
 class Search extends React.Component {
 
+  constructor() {
+    super(...arguments)
+
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent() {
+    console.log('loadComponent')
+    // 返回 promise
+    import('./text.js').then(Text => {
+      this.setState({
+        Text:Text.default
+      })
+    })
+  }
+
   render() {
     console.log(common())
+    const { Text } = this.state;
     return <div className="search-text">
-      Search Text 12222 <img src={ logo } />
+      {
+        Text ? <Text /> : null
+      }
+      Search Text 12222 <img src={ logo } onClick={ this.loadComponent.bind(this) }/>
       
     </div>
     
